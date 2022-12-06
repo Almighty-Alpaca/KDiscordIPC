b5883fb4023934f30cd06e2df591eac9dc52d48f
Aljoscha Grebe <aljoschagrebe@gmail.com>
2022-12-06 19:34:46 +0100

    feat: make KDiscordIPC::on not suspending
    This allows the methods to be called from a constructor

--- src/main/kotlin/dev/cbyrne/kdiscordipc/KDiscordIPC.kt
+++ src/main/kotlin/dev/cbyrne/kdiscordipc/KDiscordIPC.kt
@@ -98,13 +98,13 @@ class KDiscordIPC(
     }
 
     @JvmName("onEvent")
-    suspend inline fun <reified T : Event> on(noinline consumer: suspend T.() -> Unit) =
+    inline fun <reified T : Event> on(noinline consumer: suspend T.() -> Unit) =
         events.filterIsInstance<T>().onEach { event ->
             scope.launch { consumer(event) }
         }.launchIn(scope)
 
     @JvmName("onPacket")
-    suspend inline fun <reified T : InboundPacket> on(noinline consumer: suspend T.() -> Unit) =
+    inline fun <reified T : InboundPacket> on(noinline consumer: suspend T.() -> Unit) =
         packets.filterIsInstance<T>().onEach { event ->
             scope.launch { consumer(event) }
         }.launchIn(scope)
